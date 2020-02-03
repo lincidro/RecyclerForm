38c9282eca3cc85e08ab27384c3a6dcdefc22474
lincidro <lincidro@gmail.com>
2020-02-02 23:14:34 -0500

    Second commit fab added

--- app/src/main/java/com/example/recyclerform/MainActivity.java
+++ app/src/main/java/com/example/recyclerform/MainActivity.java
@@ -5,18 +5,22 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.recyclerform.book.adapter.RecyclerBookAdapter;
 import com.example.recyclerform.book.model.Book;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private List<Book> bookList;
     private RecyclerView rvBooks;
     private RecyclerBookAdapter recyclerBookAdapter;
+    private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView(){
         rvBooks = findViewById(R.id.rv_books);
+        floatingActionButton = findViewById(R.id.fab_add_book);
+
+        floatingActionButton.setOnClickListener(this);
     }
 
     private void initVars(){
@@ -50,5 +57,21 @@ public class MainActivity extends AppCompatActivity {
         bookList.add(new Book("Harry Potter 3", 300));
         bookList.add(new Book("Harry Potter 4", 400));
         bookList.add(new Book("Harry Potter 5", 500));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fab_add_book:
+                bookList.add(new Book("Harry Potter 6",600));
+                updateAdapterList();
+                break;
+            default:
+                Toast.makeText(this, "default", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void updateAdapterList(){
+        recyclerBookAdapter.notifyDataSetChanged();
     }
 }
